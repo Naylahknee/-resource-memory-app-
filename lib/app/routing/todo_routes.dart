b674bd4cfecdc00c305db61e1abbffb6266e5c6f
Naylahknee <95973671+Naylahknee@users.noreply.github.com>
2026-08-16 16:52:09 -0700

    refactor: route app into Resource Memory MVP screens

--- lib/app/routing/todo_routes.dart
+++ lib/app/routing/todo_routes.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
-
 import 'package:taskee/app/routing/app_route.dart';
-import '../../features/todo/presentation/pages/add/create_taskOrNote_screen.dart';
-import '../../features/todo/presentation/pages/home/home_screen.dart';
+import 'package:taskee/features/project/presentation/pages/project_match_screen.dart';
+import 'package:taskee/features/resource/presentation/pages/library_screen.dart';
+import 'package:taskee/features/resource/presentation/pages/resource_home_screen.dart';
+import 'package:taskee/features/resource/presentation/pages/save_resource_screen.dart';
 
 final List<RouteBase> todoRoutes = <RouteBase>[
   GoRoute(
     path: Routes.homeScreen,
-    builder: (BuildContext context, GoRouterState state) {
-      return const HomeScreen();
-    },
+    builder: (BuildContext context, GoRouterState state) =>
+        const ResourceHomeScreen(),
     routes: <RouteBase>[
       GoRoute(
-        path: Routes.addScreen,
-        builder: (BuildContext context, GoRouterState state) {
-          return const CreateTaskOrNoteScreen();
-        },
+        path: Routes.saveResourceScreen,
+        builder: (BuildContext context, GoRouterState state) =>
+            const SaveResourceScreen(),
+      ),
+      GoRoute(
+        path: Routes.libraryScreen,
+        builder: (BuildContext context, GoRouterState state) =>
+            const LibraryScreen(),
+      ),
+      GoRoute(
+        path: Routes.projectMatchScreen,
+        builder: (BuildContext context, GoRouterState state) =>
+            const ProjectMatchScreen(),
       ),
     ],
   ),
