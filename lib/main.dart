c69af39595b8c369768dc50a6f216bcee25eb9e8
Naylahknee <95973671+Naylahknee@users.noreply.github.com>
2026-08-16 22:47:21 -0700

    Simplify app bootstrap to Resource Memory MVP

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,29 @@
 import 'package:flutter/material.dart';
-
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:taskee/app/theme/app_theme.dart';
-import 'package:taskee/features/note/data/datasource/hive_note_local_datasource.dart';
-import 'package:taskee/features/note/presentation/bloc/note_bloc.dart';
-import 'package:taskee/features/note/presentation/bloc/note_event.dart';
-import 'package:taskee/features/shared/cubit/tab_cubit.dart';
-import 'package:taskee/features/todo/data/datasource/notification_service.dart';
-import 'package:taskee/features/todo/presentation/bloc/todo_bloc.dart';
 import 'package:taskee/app/routing/go_router.dart';
-import 'package:taskee/features/todo/presentation/bloc/todo_event.dart';
-import 'package:taskee/di/dependency_injection.dart';
-import 'package:taskee/features/todo/data/datasource/hive_todo_local_datasource.dart';
+import 'package:taskee/app/theme/app_theme.dart';
 import 'package:taskee/features/resource/data/resource_store.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
-
-  final hiveTodoLocalDataSource = HiveTodoLocalDataSource();
-  await hiveTodoLocalDataSource.initialize();
-
-  final hiveNoteLocalDatasource = HiveNoteLocalDatasource();
-  await hiveNoteLocalDatasource.initialize();
   await ResourceStore.initialize();
-  await NotificationService().init();
-
-  configureDependencies(
-    hiveTodoLocalDataSource: hiveTodoLocalDataSource,
-    hiveNoteLocalDatasource: hiveNoteLocalDatasource,
-  );
-
-  final prefs = await SharedPreferences.getInstance();
-  final savedTab = prefs.getInt('selected_tab') ?? 0;
-
-  runApp(MyApp(initialTab: savedTab));
+  runApp(const ResourceMemoryApp());
 }
 
-class MyApp extends StatefulWidget {
-  final int initialTab;
-  const MyApp({super.key, required this.initialTab});
-
-  @override
-  MyAppState createState() => MyAppState();
-}
-
-class MyAppState extends State<MyApp> {
-  final TodoBloc todoBloc = getIt<TodoBloc>();
-  final NoteBloc noteBloc = getIt<NoteBloc>();
-
-  @override
-  void dispose() {
-    Hive.close();
-    super.dispose();
-  }
+class ResourceMemoryApp extends StatelessWidget {
+  const ResourceMemoryApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (_) => TabCubit(initialTab: widget.initialTab)),
-        BlocProvider(create: (_) => todoBloc..add(TodoStartedEvent())),
-        BlocProvider(create: (_) => noteBloc..add(NoteStartedEvent())),
-      ],
-      child: GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-        child: MaterialApp.router(
-          title: 'Resource Memory',
-          theme: AppTheme.dark,
-          debugShowCheckedModeBanner: false,
-          routerConfig: goRouter,
-        ),
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: MaterialApp.router(
+        title: 'Resource Memory',
+        theme: AppTheme.dark,
+        debugShowCheckedModeBanner: false,
+        routerConfig: goRouter,
       ),
     );
   }
