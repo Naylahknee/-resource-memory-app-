50ae7491b3e379abff59759b8a40ceee8857da05
Naylahknee <95973671+Naylahknee@users.noreply.github.com>
2026-08-17 11:05:10 -0700

    Receive shared resources while app is open or closed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 import 'package:taskee/app/routing/go_router.dart';
 import 'package:taskee/app/theme/app_theme.dart';
+import 'package:taskee/features/resource/data/incoming_share_service.dart';
 import 'package:taskee/features/resource/data/resource_store.dart';
 
 Future<void> main() async {
@@ -11,8 +15,49 @@ Future<void> main() async {
   runApp(const ResourceMemoryApp());
 }
 
-class ResourceMemoryApp extends StatelessWidget {
+class ResourceMemoryApp extends StatefulWidget {
   const ResourceMemoryApp({super.key});
+
+  @override
+  State<ResourceMemoryApp> createState() => _ResourceMemoryAppState();
+}
+
+class _ResourceMemoryAppState extends State<ResourceMemoryApp> {
+  StreamSubscription<List<SharedMediaFile>>? _shareSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _listenForShares();
+  }
+
+  Future<void> _listenForShares() async {
+    _shareSubscription = ReceiveSharingIntent.instance.getMediaStream().listen(
+      _consumeSharedItems,
+      onError: (_) {},
+    );
+
+    try {
+      final initial = await ReceiveSharingIntent.instance.getInitialMedia();
+      await _consumeSharedItems(initial);
+      await ReceiveSharingIntent.instance.reset();
+    } catch (_) {}
+  }
+
+  Future<void> _consumeSharedItems(List<SharedMediaFile> items) async {
+    if (items.isEmpty) return;
+    final saved = await IncomingShareService.saveSharedItems(items);
+    if (saved > 0) {
+      goRouter.go('/library');
+    }
+    await ReceiveSharingIntent.instance.reset();
+  }
+
+  @override
+  void dispose() {
+    _shareSubscription?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
