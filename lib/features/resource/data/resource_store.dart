19cea0576a676857ba7cbc33c607b70b30520736
Naylahknee <95973671+Naylahknee@users.noreply.github.com>
2026-08-16 22:14:29 -0700

    Add local Hive resource store

---
+++ lib/features/resource/data/resource_store.dart
@@ -0,0 +1,30 @@
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:taskee/features/resource/domain/resource.dart';
+
+class ResourceStore {
+  static const boxName = 'resource_memory_resources';
+
+  static Future<void> initialize() async {
+    if (!Hive.isBoxOpen(boxName)) {
+      await Hive.openBox<Map>(boxName);
+    }
+  }
+
+  static Box<Map> get box => Hive.box<Map>(boxName);
+
+  static List<Resource> getAll() {
+    final resources = box.values
+        .map((value) => Resource.fromMap(Map<String, dynamic>.from(value)))
+        .toList();
+    resources.sort((a, b) => b.savedAt.compareTo(a.savedAt));
+    return resources;
+  }
+
+  static Future<void> save(Resource resource) async {
+    await box.put(resource.id, resource.toMap());
+  }
+
+  static Future<void> remove(String id) async {
+    await box.delete(id);
+  }
+}
