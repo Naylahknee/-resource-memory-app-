04a8bc2a29fdeacbbc4ab2cb9a2e662644c5d559
Naylahknee <95973671+Naylahknee@users.noreply.github.com>
2026-09-01 14:05:45 -0700

    Add voice memory route

--- lib/app/routing/app_route.dart
+++ lib/app/routing/app_route.dart
@@ -1,6 +1,7 @@
 class Routes {
   static const homeScreen = '/';
   static const saveResourceScreen = 'save-resource';
+  static const voiceMemoryScreen = 'voice-memory';
   static const libraryScreen = 'library';
   static const projectMatchScreen = 'project-match';
   static const syncScreen = 'sync';
