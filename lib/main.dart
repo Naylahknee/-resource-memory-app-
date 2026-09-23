ffb2210c97c1a00994e603ccef7475e1ae01c017
Naylahknee <95973671+Naylahknee@users.noreply.github.com>
2026-09-23 16:09:05 -0700

    Initialize commitment memory store

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 import 'package:taskee/app/routing/go_router.dart';
 import 'package:taskee/app/theme/app_theme.dart';
+import 'package:taskee/features/commitment/data/commitment_store.dart';
 import 'package:taskee/features/resource/data/cloud_sync_service.dart';
 import 'package:taskee/features/resource/data/incoming_share_service.dart';
 import 'package:taskee/features/resource/data/resource_store.dart';
@@ -14,6 +15,7 @@ Future<void> main() async {
   await Hive.initFlutter();
   await CloudSyncService.initialize();
   await ResourceStore.initialize();
+  await CommitmentStore.initialize();
   if (CloudSyncService.isSignedIn) {
     await ResourceStore.syncNow();
   }
