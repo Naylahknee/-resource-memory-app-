7af8ba8427bc83eb898d925debf4f388d6c78ddc
Naylahknee <95973671+Naylahknee@users.noreply.github.com>
2026-09-23 16:27:35 -0700

    Wire spoken reminder settings

--- lib/app/routing/resource_routes.dart
+++ lib/app/routing/resource_routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 import 'package:taskee/app/routing/app_route.dart';
 import 'package:taskee/features/commitment/presentation/pages/commitments_screen.dart';
+import 'package:taskee/features/commitment/presentation/pages/spoken_reminder_settings_screen.dart';
 import 'package:taskee/features/project/presentation/pages/project_match_screen.dart';
 import 'package:taskee/features/resource/presentation/pages/install_app_screen.dart';
 import 'package:taskee/features/resource/presentation/pages/library_screen.dart';
@@ -17,6 +18,7 @@ final List<RouteBase> resourceRoutes = <RouteBase>[
     GoRoute(path: Routes.voiceMemoryScreen, builder: (context, state) => const VoiceMemoryScreen()),
     GoRoute(path: Routes.libraryScreen, builder: (context, state) => const LibraryScreen()),
     GoRoute(path: Routes.commitmentsScreen, builder: (context, state) => const CommitmentsScreen()),
+    GoRoute(path: Routes.spokenReminderSettingsScreen, builder: (context, state) => const SpokenReminderSettingsScreen()),
     GoRoute(path: 'resource/:id', builder: (context, state) => ResourceDetailScreen(resourceId: state.pathParameters['id']!)),
     GoRoute(path: Routes.projectMatchScreen, builder: (context, state) => const ProjectMatchScreen()),
     GoRoute(path: Routes.syncScreen, builder: (context, state) => const SyncScreen()),
